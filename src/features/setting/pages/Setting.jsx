75f677e533eb2e9dc7d44c9d5506d37867fbb361
Tuan-Thanh <thanhphantuan1110@gmail.com>
2024-04-18 15:59:14 +0700

    feat(setting): handle log out with ConfirmDialog

--- src/features/setting/pages/Setting.jsx
+++ src/features/setting/pages/Setting.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
 import { clearUser } from '../../authentication';
 import { useDispatch } from 'react-redux';
-import { GoogleSignin} from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
 import { emailPasswordSignOut, googleSignOut } from '../../authentication';
+import ConfirmDialog from '../../../components/ConfirmDialog';
 
 function Setting(props) {
     const dispatch = useDispatch();
+    const [dialogVisible, setDialogVisible] = useState(false);
+
     const handleSignOut = async () => {
         const user = auth().currentUser;
         if (user) {
@@ -24,7 +26,14 @@ function Setting(props) {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
             <Text>Settings!</Text>
-            <Button title='Log out' onPress={handleSignOut} />
+            <Button title='Log out' onPress={() => setDialogVisible(true)} />
+            <ConfirmDialog
+                visible={dialogVisible}
+                title="Confirm Logout"
+                message="Are you sure you want to logout?"
+                onConfirm={handleSignOut}
+                onCancel={() => setDialogVisible(false)}
+            />
         </View>
     );
 }

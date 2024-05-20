2d89239b9979a126d7d1390483bc38fc1aa82132
Tuan-Thanh <thanhphantuan1110@gmail.com>
2024-05-20 22:53:47 +0700

    feat(Investment): fake data investment

--- src/features/budget/components/InvestmentList.jsx
+++ src/features/budget/components/InvestmentList.jsx
@@ -10,48 +10,48 @@ import { setCurrentInvestmentCRUDAction, setUpdateInvestmentBottomSheetDisplay }
 import { useTranslation } from 'react-i18next'
 const investmentList = [
     {
-        id: '1',
-        name: 'Investment 1',
-        totalInterest: 2450000,
-        principal: 10000000,
-        type: 'fixed',
-        dueDay: '31/03/2024',
+      id: '6',
+      name: 'Investment 6',
+      totalInterest: 1500000,
+      principal: 5000000,
+      type: 'fixed',
+      dueDay: '01/06/2025',
     },
     {
-        id: '2',
-        name: 'Investment 2',
-        totalInterest: 24500000,
-        principal: 10000000,
-        type: 'dynamic',
-        dueDay: '31/03/2024',
+      id: '7',
+      name: 'Investment 7',
+      totalInterest: 8000000,
+      principal: 20000000,
+      type: 'dynamic',
+      dueDay: '15/08/2024',
     },
     {
-        id: '3',
-        name: 'Investment 3',
-        totalInterest: 245000,
-        principal: 1000000,
-        type: 'fixed',
-        dueDay: '31/03/2024',
+      id: '9',
+      name: 'Investment 9',
+      totalInterest: 5200000,
+      principal: 18000000,
+      type: 'dynamic',
+      dueDay: '27/02/2025',
     },
     {
-        id: '4',
-        name: 'Investment 4',
-        totalInterest: 2450000,
-        principal: 1000000,
-        type: 'dynamic',
-        dueDay: '31/03/2024',
+      id: '8',
+      name: 'Investment 8',
+      totalInterest: 350000,
+      principal: 1200000,
+      type: 'fixed',
+      dueDay: '30/11/2023', 
     },
     {
-        id: '5',
-        name: 'Investment 5',
-        totalInterest: 2450000,
-        principal: 1000000,
-        type: 'dynamic',
-        dueDay: '31/03/2024',
+      id: '10',
+      name: 'Investment 10',
+      totalInterest: 1700000,
+      principal: 7000000,
+      type: 'fixed',
+      dueDay: '10/12/2024',
     },
-]
+  ];
 
-const InvestmentList = () => {
+const InvestmentList = (props) => {
     const dispatch = useDispatch();
     const { t } = useTranslation();
     return (
